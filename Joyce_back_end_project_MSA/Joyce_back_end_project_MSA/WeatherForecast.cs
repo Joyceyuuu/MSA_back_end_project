f80f95f593524c8f27d2d3464bd2abd67cfa6d02
Joyceyuuu <Joyceyu54@gmail.com>
2022-08-06 23:38:34 +1200

    create back end project

---
+++ Joyce_back_end_project_MSA/Joyce_back_end_project_MSA/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Joyce_back_end_project_MSA
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
