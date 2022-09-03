e0a6ed506fc0cbf49ebc1763c4a803a9db951e53
Joyceyuuu <Joyceyu54@gmail.com>
2022-09-04 00:54:29 +1200

    added entity frame work package and CORS

--- Joyce_back_end_project_MSA/Joyce_back_end_project_MSA/Program.cs
+++ Joyce_back_end_project_MSA/Joyce_back_end_project_MSA/Program.cs
@@ -22,8 +22,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
 var app = builder.Build();
 
+app.UseCors("corsapp");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
